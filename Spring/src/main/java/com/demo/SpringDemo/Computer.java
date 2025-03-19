bde8284663c57f308b63a8dd93ee87a8f62cf12a
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-03-19 20:49:56 +0530

    Saurabh Add . Spring learnings

---
+++ Spring/src/main/java/com/demo/SpringDemo/Computer.java
@@ -0,0 +1,5 @@
+package com.demo.SpringDemo;
+
+public interface Computer {
+     void compile();
+}
---
+++ Spring/src/main/java/com/demo/SpringDemo/Desktop.java
@@ -0,0 +1,8 @@
+package com.demo.SpringDemo;
+
+public class Desktop implements Computer {
+
+    public void compile(){
+        System.out.println("Compiling desktop...");
+    }
+}
